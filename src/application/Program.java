07755fbba8a71a4dcd6ba87bd47fe86c406d0b58
joaovrds15 <joaovictorodrigues4009@gmail.com>
2020-05-13 15:39:01 -0300

    Bad solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,20 +12,35 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
-		System.out.println("Room number:");
+		System.out.print("Room number:");
 		int roomNumber = sc.nextInt();
 		System.out.print("Check-in date (dd/MM/yyyy):");
 		Date checkIn = sdf.parse(sc.next());
-		System.out.println("Check-out: ");
+		System.out.print("Check-out: ");
 		Date checkOut = sdf.parse(sc.next());
 		
-		if (!checkOut.after(checkOut)) {
-			System.out.println("Error in reservation: Reservation dates for update must be future dates\r\n" + 
+		if (!checkOut.after(checkIn)) {
+			System.out.println("Error in reservation: Reservation dates for update must be future dates" + 
 					"");
 		}
 		else {
 			Reservation reservation = new Reservation(roomNumber,checkIn,checkOut);
+			System.out.println();
+			System.out.println(reservation);
+			System.out.println("Enter data to update the reservation");
+			System.out.print("Check in date:");
+			checkIn = sdf.parse(sc.next());
+			System.out.print("Check out date:");
+			checkOut = sdf.parse(sc.next());
+			String error = reservation.updateDates(checkIn, checkOut);
+			if (error != null) {
+				System.out.println("Reservation error:"+ error);
+			}
+			
+			else {
+				System.out.println(reservation);
+			}
 		}
-		
+		sc.close();
 	}
-}
+}	
